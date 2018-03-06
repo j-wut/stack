88e74e4f0fe57ee7c5b1dee80802f3fc1203f92c
J-wut <james.wut@gmail.com>
2018-03-06 15:49:40 -0500

    looks much better

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import KeyHandler, {KEYPRESS} from 'react-key-handler';
+import black from './black.png';
+import white from './white.png';
 import logo from './logo.svg';
 import './App.css';
 
 
 function Block(props){
-  return props.value;
+  switch(props.value){
+    case 1:return <img src={black} alt="block"/>;
+    default:return <img src={white} alt="blank"/>;
+  }
 }
 function Row(props){
   if(props.values.length > 0){
   var ret = [];
   for(var i=0;i<props.values.length;i++)
     ret.push(<Block value={props.values[i]}/>);
-  return(<div>{ret}</div>);
+  return(<div className="row">{ret}</div>);
   }
   return;
 }
@@ -21,20 +26,21 @@ class Game extends Component{
   constructor(props){
     super(props);
     this.state = {
-      width: 100,
+      width: 20,
       dir: true, 
-      ind: 1,
-      length: 20,
+      ind: 6,
+      length: 4,
       game: [],
+      delay: 100,
     }
-    this.timeInterval = setInterval(this.update.bind(this), 50);
+    setTimeout(this.update.bind(this),this.state.delay);
   }
 
   dropRow(){
     var game = this.state.game.slice();
-    var crow=Array(this.state.width).fill("__");
+    var crow=Array(this.state.width).fill(0);
     for(var i = 0;i<this.state.length;i++){
-      crow[i+this.state.ind]="H";
+      crow[i+this.state.ind]=1;
     }
     if(game.length==0){
       game.push(crow);
@@ -44,21 +50,25 @@ class Game extends Component{
     var miss=0;
     for(i = 0;i<this.state.length;i++){
       if(crow[i+this.state.ind]!=game[0][i+this.state.ind]){
-        crow[i+this.state.ind]="__";
+        crow[i+this.state.ind]=0;
         miss++;
       }
     }
     game.unshift(crow);
-    this.setState({game:game,length:this.state.length - miss,ind:this.state.width - this.state.length - 1});
-    clearInterval(this.timeInterval);
-    this.timeInterval = setInterval(this.update.bind(this), Math.max(10,50-(game.length/2)*5));
+    var ind;
+    var dir = Math.random()>0.5;
+    if(dir){
+      ind=1;
+    }else{
+      ind=this.state.width-this.state.length-1;
+    }
+    this.setState({game:game,dir:dir,length:this.state.length - miss,ind:ind});
+    this.setState({delay:this.state.delay-1});
   }
 
   update(){
     if(this.state.length==0){ //game over
-      this.setState({ind:Math.round(Math.random()*80),length:20,game:[]});
-      clearInterval(this.timeInterval);
-      this.timeInterval = setInterval(this.update.bind(this),50);
+      this.setState({ind:1+Math.round(Math.random()*5),length:4,game:[],delay:50});
     }
     var next;
     var dir = this.state.dir;
@@ -72,13 +82,13 @@ class Game extends Component{
     }
 
     this.setState({dir:dir,ind:next});
-    return;
+    setTimeout(this.update.bind(this),this.state.delay);
   }
 
   render(){
-    var crow=Array(this.state.width).fill("__");
+    var crow=Array(this.state.width).fill(0);
     for(var i = 0;i<this.state.length;i++){
-      crow[i+this.state.ind]="H";
+      crow[i+this.state.ind]=1;
     }
     var rend = [];
     rend.push(<div><Row values={crow}/></div>);
@@ -86,7 +96,7 @@ class Game extends Component{
        rend.push(<div><Row values={this.state.game[i]}/></div>);
     
     return (
-      <div>
+      <div className="grid">
         <KeyHandler keyEventName={KEYPRESS} keyValue="ArrowDown" onKeyHandle={this.dropRow.bind(this)} /> 
         {rend}
       </div>
